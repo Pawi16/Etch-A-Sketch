bbcc9c7284ecc7dae7be041ba753178ed17069dc
Pawin <captainsuperman2011@gmail.com>
2022-05-23 17:52:41 +0700

    Create 16x16 grid

---
+++ js/script.js
@@ -0,0 +1,10 @@
+
+const container = document.querySelector('.container');
+
+
+for(let i=0;i<16*16;i++){
+  const div = document.createElement('div');
+  div.classList.add('grid');
+  container.appendChild(div);
+  
+}
