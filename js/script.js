ff4fd9d95ee1f2438a2a0d9754a2cc6917bd774a
Pawin <captainsuperman2011@gmail.com>
2022-05-23 18:14:29 +0700

    Set a hover effect to div

--- js/script.js
+++ js/script.js
@@ -2,9 +2,23 @@
 const container = document.querySelector('.container');
 
 
-for(let i=0;i<16*16;i++){
+for (let i = 0; i < 16 * 16; i++) {
   const div = document.createElement('div');
   div.classList.add('grid');
+  div.addEventListener('mouseover',(e) => {
+    e.target.classList.add('hover');
+  })
   container.appendChild(div);
+}
+
+function createGrid(){
   
+  for (let i = 0; i < 16 * 16; i++) {
+    const div = document.createElement('div');
+    div.classList.add('grid');
+    div.addEventListener('mouseover',(e) => {
+      e.target.classList.add('hover');
+    })
+    container.appendChild(div);
+  }
 }
