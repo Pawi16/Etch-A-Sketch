dd925966c9240a2c4aebe8e8615e2ba2b86ba905
Pawin <captainsuperman2011@gmail.com>
2022-05-23 19:37:11 +0700

    Add a reset button

--- js/script.js
+++ js/script.js
@@ -1,24 +1,36 @@
 
 const container = document.querySelector('.container');
 
-
-for (let i = 0; i < 16 * 16; i++) {
-  const div = document.createElement('div');
-  div.classList.add('grid');
-  div.addEventListener('mouseover',(e) => {
-    e.target.classList.add('hover');
-  })
-  container.appendChild(div);
-}
-
-function createGrid(){
-  
-  for (let i = 0; i < 16 * 16; i++) {
+function createGrid(sqNum){
+  for (let i = 0; i < sqNum * sqNum; i++) {
     const div = document.createElement('div');
     div.classList.add('grid');
+    div.style.width = `${500/sqNum}px`;
+    div.style.height = `${500/sqNum}px`;
     div.addEventListener('mouseover',(e) => {
       e.target.classList.add('hover');
     })
     container.appendChild(div);
   }
 }
+
+function removeAllChildNodes(parent){
+  while (parent.firstChild) {
+    parent.removeChild(parent.firstChild);
+  }
+}
+
+createGrid(16);
+
+const btn = document.querySelector('.btn');
+btn.addEventListener('click',() => {
+  let sqNum = prompt("number of squares per side",0);
+  if (sqNum>100) {
+    return;
+  }
+  removeAllChildNodes(container);
+  createGrid(sqNum);
+  
+})
+
+
